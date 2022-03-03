cd4b8970b6e2935bb112ac114fb3bfa75cd8748e
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-03-03 08:39:56 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function helloWorld(name){
+    if( name === "" || name === undefined) {
+        return "Hello, World,";
+    }
+    return "Hello, " + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() +",";
+
+}
+console.log(helloWorld(undefined));
